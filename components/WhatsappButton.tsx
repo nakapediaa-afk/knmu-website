f2645deabf78c362ef9f06e629a77d84448dfbd0
Rynn <nakapediaa@gmail.com>
2026-05-09 17:13:46 +0700

    Hompage update

--- components/WhatsappButton.tsx
+++ components/WhatsappButton.tsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function WhatsappButton() {
   return (
     <a
-      href="https://wa.me/628XXXXXXXXXX"
+      href="https://wa.me/6283832827452"
       target="_blank"
       className="fixed bottom-6 right-6 z-50 bg-[#25D366] hover:scale-110 transition text-white p-5 rounded-full shadow-2xl text-3xl"
     >
